e39ae7970d572fb28de377e06c67ad1ba1f7600f
Max Klevebrand <max.klevebrand@gmail.com>
2025-12-18 20:00:02 +0100

    Init

---
+++ src/main.cpp
@@ -0,0 +1,29 @@
+#include "klevebrand_maxjet_drone.h"
+#include "pwm_receiver.h"
+#include "./components/drone_pwm_receiver/drone_pwm_receiver.h"
+
+uint8_t motor_pin_numbers[16] = { 3, 2, 7, 6};
+
+KlevebrandMaxJetDrone drone = KlevebrandMaxJetDrone(motor_pin_numbers);
+DronePwmReceiver receiver = DronePwmReceiver(1, 4, 3, 2, 7);
+
+void setup()
+{
+  // Startup the gyroscope and motors
+  drone.setup();
+
+  // Startup the reciever
+  receiver.setup();
+}
+
+void loop()
+{
+  // Set drone flight values from the receiver
+  receiver.setThrottleYawPitchRoll(&drone);
+
+  // Set the flight mode of the drone from the receiver 
+  receiver.setFlightMode(&drone);
+
+  // Run the drone feedback-loop
+  drone.run();
+}
